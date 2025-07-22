533ee7c9a1e476a476ec24965e62ebc429c9888d
Anand <anandraj6783@gmail.com>
2025-07-22 21:10:16 +0530

    conditional statements

--- basic_logic.py
+++ basic_logic.py
@@ -1,3 +1,14 @@
 # input syntax 
-first_input = input("the way to take inputs in python (input taken is a string)\n");
+first_input = input("the way to take inputs in python (input taken is a string)\n")
 print("\n"+first_input)
+
+# conditional statements
+name = input("enter your name\n")
+
+if name == "Anand Raj":
+    print("W rizz")
+elif name == "anand raj":
+    print("L rizz")
+else :
+    print("stop gooning")
+
