1256ba5d0e1b2b56586fd928d70345aa5f4193fd
Anand <anandraj6783@gmail.com>
2025-07-22 21:40:56 +0530

    logical AND , OR

--- basic_logic.py
+++ basic_logic.py
@@ -1,7 +1,6 @@
 # input syntax 
 first_input = input("the way to take inputs in python (input taken is a string)\n")
 print("\n"+first_input)
-
 # conditional statements
 name = input("enter your name\n")
 
@@ -12,3 +11,14 @@ elif name == "anand raj":
 else :
     print("stop gooning")
 
+
+# logical AND,OR 
+screen_time = int(input("enter your screen time\n"))
+age = int(input("enter your age\n"))
+
+if screen_time >= 0 and screen_time <= 1 :
+    print("Bruh, you actually out here living?")
+elif screen_time >= 10 and age >= 25 :
+    print("ayy, we got a reddit mod here")
+elif screen_time >= 7 or age >= 20 :
+    print("full time gooner")
