c32e90d4143e6b0ffed5962d8789ca11dc86baeb
Anand <anandraj6783@gmail.com>
2025-07-24 22:20:11 +0530

    tuples

---
+++ tuples.py
@@ -0,0 +1,9 @@
+# tuples are like const list/array
+# dictionaries methods like .values,keys,items return a tuple
+# syntax
+
+my_name = ("a","n","a","n","d")
+
+my_name[2] = "change" # typeError
+
+# looping is same as a list
